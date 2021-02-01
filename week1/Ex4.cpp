ca35b079ac89ea0ccc41cd207e3a912fa143071c
Smirn0v-da <dan16533_2@mail.ru>
2021-02-01 11:06:08 +0300

    Упражнение 4

---
+++ week1/Ex4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main(){
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < i + 1; j++) {
+            cout << '*' ;
+        }
+        cout << endl;
+    }
+    return 0;
+}
