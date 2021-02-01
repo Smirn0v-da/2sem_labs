64fa85ccdd52fb772f65be16a50ed17840883569
Smir0v-da <dan16533_2@mail.ru>
2021-02-01 11:03:27 +0300

    Упражнение 3

--- week1/Ex3.cpp
+++ week1/Ex3.cpp
@@ -8,7 +8,7 @@ int main(){
     int N;
     cin >> N;
     for (int i = 0; i < N; i++) {
-        for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
             cout << '*' ;
         }
         cout << endl;
